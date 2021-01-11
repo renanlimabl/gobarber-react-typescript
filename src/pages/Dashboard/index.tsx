da7555bf367c2766ae8b7a63fff5099543d89339
renanlimabl <renanlimabl@gmail.com>
2021-01-11 18:21:45 -0300

    Adiciona hook useParams

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Dashboard: React.FC = () => <h1>BLABLLABLALBAL</h1>;
+interface IParamTypes {
+  id: string;
+}
+
+const Dashboard: React.FC = () => {
+  const { id } = useParams<IParamTypes>();
+  console.log(id);
+  return <h1>BLABLLABLALBAL</h1>;
+};
 
 export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
-    <Route path="/dashboard" isPrivate component={Dashboard} />
+    <Route path="/dashboard/:id" isPrivate component={Dashboard} />
   </Switch>
 );
 
